81a6700d67cafe3d900a8558674f615a301eac27
ishhgupta <isha.gupta@students.iiit.ac.in>
2021-10-25 00:25:56 +0530

    added new file

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Ninjas from './ninjas';
+import AddNinja from './AddNinja';
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    ninjas : [
+      {name:'Ryu',age:24, belt:'black',id:1 },
+      {name:'misha', age:50,belt:"green",id:2},
+      {name:'crystal',age:30,belt:"pink",id:3 }
+    ]
+  }
+  addNinja = (ninja) => {
+      ninja.id = Math.random();
+      let ninjas = [...this.state.ninjas, ninja];
+      this.setState ({
+        ninjas : ninjas
+      })
+      // console.log(ninja);
+  }
+
+  deleteNinja = (id) =>{
+    // console.log(id);
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !==id
+    })
+    this.setState({
+      ninjas: ninjas
+    })
+
+  }
+  render(){
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>My first React app</h1>
+       <p>welcome:)</p>
+       <Ninjas deleteNinja = {this.deleteNinja} ninjas={this.state.ninjas}/>
+       <AddNinja addNinja = {this.addNinja}/>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
